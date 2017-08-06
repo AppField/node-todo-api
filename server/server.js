4e42a5374a13458629e21d22703a7ee2becc145d
WebField <roman@muehlfeldner.net>
2017-08-06 23:13:15 +0200

    Add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -30,6 +31,21 @@ app.get('/todos', (req, res) => {
         .catch(e => {
             res.status(400).send(e);
         });
+});
+
+// GET /todos/12345
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        res.status(400).send();
+    }
+    Todo.findById(id)
+        .then(todo => {
+            if (!todo) {
+                res.status(404).send('Unable to find todo');
+            }
+            res.send({todo});
+        }).catch(e => res.status(400).send(e))
 });
 
 
